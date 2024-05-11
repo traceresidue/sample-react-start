7d3080dec8dd4b5eb3dedfae734ecd4dda37c82e
COPHRI <emailthedirector@gmail.com>
2024-05-10 22:40:25 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
       </div>
 
       <h1 className="text-2xl lg:text-5xl mb-10 text-right">
-        Start React{" "}
+        Start React LUKA{" "}
         <span className="block text-lg text-blue-400">on DigitalOcean</span>
       </h1>
 
