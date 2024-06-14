88bb1d565edaa5a64badfe773d7add570d67db82
traceresidue <emailthedirector@gmail.com>
2024-06-14 17:05:18 -0500

    d
    d

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+'use client';
 
-/**
- * Uses Tailwind CSS for styling
- * Tailwind file is imported in App.css
- */
+import { useState } from 'react';
+import { getAnswer } from './actions';
 
-export default function App() {
+// Force the page to be dynamic and allow streaming responses up to 30 seconds
+export const dynamic = 'force-dynamic';
+export const maxDuration = 30;
+
+export default function Home() {
+  const [generation, setGeneration] = useState<string>('');
+
   return (
-    <div className="app min-h-screen text-blue-200 flex items-center flex-col p-20">
-      <div className="mb-10 grid grid-cols-4 grid-rows-2 w-1/2 mx-auto">
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-        <img
-          className="col-span-2 row-span-3 animate-spin m-auto"
-          style={{ animationDuration: "30s" }}
-          src={logo}
-          alt="React Logo"
-          width="20"
-        />
-        <img className="opacity-25" src={logo} alt="React Logo" width="30" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="10" />
-        <img className="opacity-25" src={logo} alt="React Logo" width="300" />
-      </div>
-
-      <h1 className="text-2xl lg:text-5xl mb-10 text-right">
-        Start React W{" "}
-        <span className="block text-lg text-blue-400">on DigitalOcean</span>
-      </h1>
-
-      <div className="grid grid-cols-2 grid-rows-2 gap-4">
-        <Button
-          text="Luka Locks"
-          url="https://www.digitalocean.com/docs/app-platform"
-        />
-        <Button
-          text="DigitalOcean Dashboard"
-          url="https://cloud.digitalocean.com/apps"
-        />
-      </div>
+    <div>
+      <button
+        onClick={async () => {
+          const { text } = await getAnswer('Why is the sky blue?');
+          setGeneration(text);
+        }}
+      >
+        Answer
+      </button>
+      <div>{generation}</div>
     </div>
-  );
-}
-
-function Button({ className, text, url = "#" }) {
-  return (
-    <a
-      href={url}
-      className={`${className} py-3 px-6 bg-purple-400 hover:bg-purple-300 text-purple-800 hover:text-purple-900 block rounded text-center shadow flex items-center justify-center leading-snug text-xs transition ease-in duration-150`}
-    >
-      {text}
-    </a>
   );
 }
